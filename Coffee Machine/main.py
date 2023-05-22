5a10b82b9907d41b2603eb267cf2c7f79add5dd4
soumyyyaaa <soumya.agarwal1103@gmail.com>
2023-05-22 08:05:26 +0530

    Coffee Machine Updated

--- Coffee Machine/main.py
+++ Coffee Machine/main.py
@@ -23,84 +23,59 @@ MENU = {
         "cost": 3.0,
     }
 }
+
 resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
 }
 
-profit = 0
+drinkDispensed = True
+money = 0
+enoughResource = False
+moneyInserted = 0
 
-def report():
-    print(f"Water: {resources['water']}ml")
-    print(f"Milk: {resources['milk']}ml")
-    print(f"Coffee: {resources['coffee']}g")
-    print(f"Money: ${profit}")
-
-
-def process_coins():
-    print("Please insert coins.")
-    quarters = int(input("How many quarters?: "))
-    dimes = int(input("How many dimes?: "))
-    nickles = int(input("How many nickles?: "))
-    pennies = int(input("How many pennies?: "))
-    money = 0.25 * quarters + 0.10 * dimes + 0.05 * nickles + 0.01 * pennies
-    return money
-
-
-def transaction(money_added, coffee):
-    global profit
-    if money_added == MENU[coffee]["cost"]:
-        profit += MENU[coffee]["cost"]
-    elif money_added > MENU[coffee]["cost"]:
-        profit += MENU[coffee]["cost"]
-        refund_money = round(money_added - MENU[coffee]["cost"], 2)
-        print(f"Here is ${refund_money} dollars in change.")
-        print(f"Here is your {coffee}☕. Enjoy!")
-    else:
-        print("Sorry not enough money. Money refunded.")
-
-
-def coffee_making(chosen_coffee):
-    if resources["water"] >= MENU[chosen_coffee]["ingredients"]["water"]:
-        water = True
-    else:
-        water = False
-
-    if resources["coffee"] >= MENU[chosen_coffee]["ingredients"]["coffee"]:
-        coffee = True
-    else:
-        coffee = False
-
-    if chosen_coffee == "latte" or chosen_coffee == "cappuccino":
-        if resources["milk"] >= MENU[choice]["ingredients"]["milk"]:
-            milk = True
-        else:
-            milk = False
-
-        if milk:
-            resources["milk"] -= MENU[choice]["ingredients"]["milk"]
-        else:
-            print("Sorry there is not enough milk.")
-            return
+while drinkDispensed:
+    userChoice = input("What would you like? (espresso/latte/cappuccino): ")
+        
+    if userChoice == "off":
+        drinkDispensed = False
+        
+    elif userChoice == "report":
+        print(f"Water : {resources['water']}ml \nMilk : {resources['milk']}ml \nCoffee : "
+              f"{resources['coffee']}g \nMoney : ${money}")
     
-    if water and coffee:
-        resources["water"] -= MENU[chosen_coffee]["ingredients"]["water"]
-        resources["coffee"] -= MENU[chosen_coffee]["ingredients"]["coffee"]
-        money = process_coins()
-        transaction(money, chosen_coffee)
-    elif water:
-        print("Sorry there is not enough coffee.")
     else:
-        print("Sorry there is not enough water.")
+        for key in MENU[userChoice]["ingredients"]:
+            if MENU[userChoice]["ingredients"][key] > resources[key]:
+                print(f"Sorry there is not enough {key}.")
+                break
+            else:
+                enoughResource = True
+                break
 
+        if enoughResource:
+            print("Please insert coins.")
+            quarter = float(input("How many quarters?: "))
+            dime = float(input("How many dimes?: "))
+            nickle = float(input("How many nickles?: "))
+            penny = float(input("How many pennies?: "))
+            moneyInserted = 0.25 * quarter + 0.10 * dime + 0.05 * nickle + 0.01 * penny
 
-coffee_dispenser = True
-while coffee_dispenser:
-    choice = input("What would you like? (espresso/latte/cappuccino): ")
-    if choice == "off":
-        coffee_dispenser = False
-    elif choice == "report":
-        report()
-    else:
-        coffee_making(choice)
+            transactionSuccessful = False
+            if moneyInserted < MENU[userChoice]["cost"]:
+                print("Sorry that's not enough money. Money refunded.")
+
+            elif moneyInserted > MENU[userChoice]["cost"]:
+                transactionSuccessful = True
+                money += MENU[userChoice]["cost"]
+                print(f"Here is ${round(moneyInserted - MENU[userChoice]['cost'], 2)} dollars in change.")
+
+            else:
+                transactionSuccessful = True
+                money += MENU[userChoice]["cost"]
+
+            if transactionSuccessful:
+                for key in MENU[userChoice]["ingredients"]:
+                    resources[key] -= MENU[userChoice]["ingredients"][key]
+                print(f"Here is your {userChoice}☕. Enjoy!")
